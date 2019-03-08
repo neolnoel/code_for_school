9c3e8b8dd4dcc05a185f30d5d972e6ec94f97431
mmjax <33595233+mmjax@users.noreply.github.com>
2019-03-08 23:22:21 +0300

    написание main.c, прописаны: сложение, вычетание, умножение, деление, ошибки

---
+++ Задание №0/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "CalcFunctions.c"
+
+int main(){
+    int error;
+    double Num1, Num2;
+    char sign;
+
+    while (sign != "E"){
+        scanf("%f %c %f", &Num1, &sign, &Num2);
+        switch(sign){
+            case '+':
+                printf("%f + %f = %f", Num1, Num2, Num1 + Num2);
+                error = 0;
+                break;
+            case '-':
+                printf("%f - %f = %f", Num1, Num2, Num1 - Num2);
+                error = 0;
+                break;
+            case '*':
+                printf("%f * %f = %f", Num1, Num2, Num1 * Num2);
+                error = 0;
+                break;
+            case '/':
+                printf("%f / %f = %f", Num1, Num2, dev(Num1, Num2, error));
+                break;
+            default:
+                error = 2;
+        }
+        switch (error){
+            case 2:
+                printf("invalid input\n");
+                break;
+            case 1:
+                printf("cannot be divided by 0\n");
+                break;
+        }
+    }
+    return 0;
+}
